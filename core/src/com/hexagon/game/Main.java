a4ead951f6cd49f8412639f190876ff6287744b7
Johannes Lüke <johannes.lueke@siemens.com>
2017-12-18 12:53:05 +0100

    basic stuff

--- core/src/com/hexagon/game/Main.java
+++ core/src/com/hexagon/game/Main.java
@@ -1,33 +1,40 @@
 package com.hexagon.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class Main extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+import de.svdragster.logica.system.SystemMessageDelivery;
+import de.svdragster.logica.world.Engine;
+import sun.rmi.runtime.Log;
+
+public class Main extends Game {
+
+	private Engine	Logic;
+
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		Logic = new Engine();
+
+		Logic.getSystemManager().addSystem(new SystemMessageDelivery(Logic.getEntityManager()));
+
 	}
 
 	@Override
 	public void render () {
+		/**/
+		Logic.run();
+		/**/
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
 	}
 }
